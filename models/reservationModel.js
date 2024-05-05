8512c586a399e87a1e623ffcc4b744c9e5edf69e
ashriya <ashriyamanandhar28@gmail.com>
2024-05-05 11:52:49 +0545

    assignment

---
+++ models/reservationModel.js
@@ -0,0 +1,13 @@
+// models/Reservation.js
+
+const mongoose = require("mongoose");
+
+const reservationSchema = new mongoose.Schema({
+  userId: { type: Number, required: true },
+  eventDate: { type: Date, required: true },
+  numberOfGuests: { type: Number, required: true },
+});
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+
+module.exports = Reservation;
